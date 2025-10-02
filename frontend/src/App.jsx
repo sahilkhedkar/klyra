3d55b08a3533b08b0be15911f8acf605645f2878
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-10-02 13:55:26 +0530

    initiallized the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
   return (
     <>
-     
+     <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/send" element={<Send/>}/>
+      </Routes>
+     </BrowserRouter>
     </>
   )
 }
