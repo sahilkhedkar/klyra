ff07a18b3d263227033b329630c084f0f21538b5
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-10-02 15:03:03 +0530

    created bottomwarning and button component

---
+++ frontend/src/components/BottomWarning.jsx
@@ -0,0 +1,12 @@
+export const BottomWarning = ({label, buttonText, to}) => {
+    return (
+        <div className="py-2 text-sm flex justify-center">
+            <div>
+                {label}
+            </div>
+            <Link className="pointer underline pl-1" to={to}>
+            {buttonText}
+            </Link>
+        </div>
+    )
+}
