dd3d2a088193f86b2dce62c89ab1eca975be82a7
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-10-02 15:10:14 +0530

    made some changes

--- frontend/src/components/SubHeading.jsx
+++ frontend/src/components/SubHeading.jsx
@@ -1,7 +1,3 @@
-export const SubHeading = ({label}) => {
-    return (
-        <div className="text-slate-500 text-md pt-1 px-4 pb-4">
-            {label}
-        </div>
-    )
-}
+export const SubHeading = ({ label }) => {
+  return <div className="text-slate-500 text-md pt-1 px-4 pb-4">{label}</div>;
+};
